0455f2e04d338fa3115caec28b5b39ba485555c3
benj-G <benjamin.gottheil@sjsu.edu>
2016-11-07 16:09:06 -0800

    added deletion of both phpsession and c_id cookies

--- faceTracker/php/logout.php
+++ faceTracker/php/logout.php
@@ -8,7 +8,18 @@
 
     // Ending client session
     end_session();
-
+    //courtest of http://stackoverflow.com/questions/686155/remove-a-cookie
+    if (isset($_COOKIE['c_sId'])) {
+        unset($_COOKIE['c_sId']);
+       setcookie('c_sId', '', time() - 3600, '/'); // empty value and old timestamp
+       // die("in loop");
+    }
+    if (isset($_COOKIE['PHPSESSID'])) {
+        unset($_COOKIE['PHPSESSID']);
+       setcookie('PHPSESSID', '', time() - 3600, '/'); // empty value and old timestamp
+       // die("in loop");
+    }
+    //else die("what?");
     // Redirect them to the landing page
     header("Location: ../index.html");
     
