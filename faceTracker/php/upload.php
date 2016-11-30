c6bc0634fd74b1508b6433f2d545084111160bb6
benj-G <benjamin.gottheil@sjsu.edu>
2016-11-30 01:27:41 -0800

    changed to link to cgi

--- faceTracker/php/upload.php
+++ faceTracker/php/upload.php
@@ -3,7 +3,7 @@
     include "db_utils.php";
     include "post_utils.php";
 
-    $dir = "../data/vid/";
+    $dir = "../../data/vid/";
     $filename = $dir . basename($_FILES["file"]["name"]);
     if(isset($_COOKIE['c_sId'])){
      $cookie_sId = clean_post($_COOKIE['c_sId']);
@@ -11,17 +11,31 @@
     }
 
     $user_name = get_username_from_sessions($cookie_sId);
+    if(strlen($user_name) == 0) die("username 0");
     // If submit value is set, validate file type and extract file info
     // Uncomment this
     //if(isset($_POST["submit"])) {
     //
     //  add check for bad filename
-        $query = "INSERT INTO VIDEO_METADATA(USER_ID, SRC_VIDEO_FILE, OUT_VIDEO_FILE, NUM_FRAMES, FRAME_RATE, FRAME_WIDTH, FRAME_HEIGHT) VALUES (1, '$filename', -1, -1, -1, -1, -1)";
         $dbConn = pgConnect();
+        $query = "SELECT USER_ID FROM USER_PROFILES WHERE USER_NAME='$user_name'";
+        $results =  pgQuery($dbConn, $query);
+        $tuple = pg_fetch_assoc($results);
+        if(count($tuple) == 0) die("invalid user");// leave connections open?
+        else{
+            $user_id = $tuple['user_id'];
+        }
+        $query = "INSERT INTO VIDEO_METADATA(USER_ID, SRC_VIDEO_FILE, OUT_VIDEO_FILE, NUM_FRAMES, FRAME_RATE, FRAME_WIDTH, FRAME_HEIGHT) VALUES ('$user_id', '$filename', -1, -1, -1, -1, -1)";
         pgQuery($dbConn, $query);
+        $query = "SELECT VIDEO_ID FROM VIDEO_METADATA where user_id='$user_id' AND src_video_file= '$filename'";
+        $results = pgQuery($dbConn, $query);
+        $tuple = pg_fetch_assoc($results);
+        if(count($tuple) == 0) die("video wasn't stored");
+        else{
+            $vID = $tuple['video_id'];
+        }
         pgDisconnect($dbConn);
-        make_cgi_post($cookie_sId, 3);
-        die("done");
+        //return;
         // Check file type with ffprobe
         // If file is valid, store file info
     //}
@@ -32,9 +46,9 @@
     }
 
     if (!is_writeable($dir)) {
-        mkdir("../data");
-        mkdir("../data/vid");
-       if (!is_writeable($dir . $_FILES['file']['name'])) die("Cannot write to destination file");
+        mkdir("../../data");
+        mkdir("../../data/vid");
+      // if (!is_writeable($dir . $_FILES['file']['name'])) die("Cannot write to destination file");
     }
     //rename("../data/vid/test.txt", "../data/vid/tmp.txt");
     // Check if name already in use
@@ -42,6 +56,7 @@
     // Attempt to upload file and echo success or failure
     if(move_uploaded_file($_FILES["file"]["tmp_name"], $filename)) {
         echo("Your file was uploaded successfully!");
+        make_cgi_post($cookie_sId, $vID);
     } else {
         echo("There was an error uploading your file. Please verify the file type and try again.");
     }
@@ -60,10 +75,11 @@
     }
 
     function make_cgi_post($session_id, $video_id){
-        $dest = "http://localhost/faceTracker/conduit.cgi";
+        $dest = "http://localhost/faceTracker/php/conduit.cgi";
         $data = array(
             'SESSIONID' => $session_id,
-            'VIDEOID' => $video_id
+            'VIDEOID' => $video_id,
+            'DETAILS' => -1
             );
         //http://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php
         $curl = curl_init($dest);
