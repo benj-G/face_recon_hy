7c24d48846ec98aab70ed7414fe2e8139011fcc9
Tom <tmryan33@gmail.com>
2016-12-03 17:12:22 -0800

    Adding dynamic user video displaying capability

--- faceTracker/php/account.php
+++ faceTracker/php/account.php
@@ -7,23 +7,18 @@
         if(is_session_valid()) {
             echo(file_to_html('../account.html'));
         } else {
-            header("Location: ../login.html");
+            header("Location: ../login.php");
             exit();
         }
     }
 
     function file_to_html($pagePath) {
-        // Note: This will eventually inject HTML using a vector of videos 
-        //       which will be built using whatever template we have
-        // Video tag test template, will eventually just call build_videos()
-        $testVid = "<video width='320' height='240' controls><source src='../data/vid/testVid.mp4' /></video>";
-        
         // Variables for substitution into HTML string
         $fname = $_SESSION['first_name'];
         $lname = $_SESSION['last_name'];
-        $subs = array('$INTRO_TEXT' => "Welcome, $fname $lname! Your videos will be posted somewhere here and other stuff.",
+        $subs = array('$INTRO_TEXT' => "Welcome, $fname $lname! Any videos tied to your account will be posted in the Videos section below.",
                       '$HEADER_TEXT' => "User Page",
-                      '$USER_VIDEOS' => $testVid);
+                      '$USER_VIDEOS' => build_videos());
         
         $htmlStr = "";
         
@@ -33,12 +28,36 @@
             trigger_error("Fatal error while attempting to load html from file.", E_USER_ERROR);
         }
         
-        // Return html from file with variable substitutions defined above
+        // Return HTML from file with variable substitutions defined above
         return strtr($htmlStr, $subs);
     }
 
-    function build_videos() {
-        // Note: This function will build our video tag using a dynamic list of vids and a template
+
+    // Retrieves video file path and builds videos HTML element
+    function build_videos() {                    
+        $videoTag = "";
+
+        // Defaulting to user_id 0 since it won't exist
+        $userId = 0;
+        
+        $videoTag = "<ul class='list-inline'>";
+        $vidSubTag = "<video width='320' height='240' controls /><source src='";
+
+        // Retrieving map of video paths belonging to current user
+        // If any videos exist for current user, then $vidMetaData
+        // contains keys 'video_id' and 'out_video_file'
+        $userId = get_current_user_id();
+        $vidMetaData = get_vid_paths($userId);
+        
+        // If user has processed videos
+        if($vidMetaData != false) {
+            // Building HTML list element for each found video
+            while($row = pgFetchAssoc($vidMetaData)) {
+                $videoTag .= "<li>" . $vidSubTag . $row['out_video_file'] . "'/></li>";
+            }
+        }
+        
+        return $videoTag .= "</ul>";
     }
 
 ?>
